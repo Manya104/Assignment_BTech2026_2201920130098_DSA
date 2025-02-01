c02f5fffd899e2a0eebadd6aef9bba84d0577b2d
Manya Johri <142681229+Manya104@users.noreply.github.com>
2025-02-01 21:31:51 +0530

    Time: 0 ms (100%), Space: 27.7 MB (61.71%) - LeetHub

---
+++ 3151-special-array-i/3151-special-array-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+
+    
+
+    bool isArraySpecial(vector<int>& arr) {
+
+        for (int i=0;i<arr.size()-1;i++){
+            if ((arr[i]%2==0 && arr[i+1]%2==0 )|| ( arr[i]%2!=0 && arr[i+1]%2!=0 )){
+                return false;
+            }
+        }
+        return true;
+        
+}
+
+
+};
